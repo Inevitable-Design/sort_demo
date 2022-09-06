ba01b4227c75f4ebc7539bc4d9031547126a67a0
Ekansh <96431359+Inevitable-Design@users.noreply.github.com>
2022-09-06 14:40:28 +0530

    Update newsort.java

--- src/newsort.java
+++ src/newsort.java
@@ -1,34 +1,16 @@
-#include <bits/stdc++.h>
-using namespace std;
-void insertionSort(int arr[], int n)
+import java.util.*;
+public class newsort
 {
-	int i, key, j;
-	for (i = 1; i < n; i++)
-	{
-		key = arr[i];
-		j = i - 1;
-		while (j >= 0 && arr[j] > key)
-		{
-			arr[j + 1] = arr[j];
-			j = j - 1;
+	public static void main(String[] args) {
+		int arr[] ={12,67,32,82,10,2,7,23,78,90,56,12,24,76,78,56};
+		int n = arr.length;
+		for(int i=1;i<n;i++){
+		    int b = i-1;
+		    int key = arr[i];
+		    while(b>=0 && arr[b]>key)  arr[b+1] = arr[b--];
+		    arr[b+1] = key;
 		}
-		arr[j + 1] = key;
+      
+		System.out.println(Arrays.toString(arr));
 	}
-}
-void printArray(int arr[], int n)
-{
-	int i;
-	for (i = 0; i < n; i++)
-		cout << arr[i] << " ";
-	cout << endl;
-}
-int newsort()
-{
-	int arr[] = { 12, 11, 13, 5, 6 };
-	int N = sizeof(arr) / sizeof(arr[0]);
-
-	insertionSort(arr, N);
-	printArray(arr, N);
-
-	return 0;
 }
